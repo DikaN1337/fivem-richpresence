aa03ec934d31245eb407537c57d721e860a6121b
DikaN <diogomiguelcandido@gmail.com>
2020-09-25 12:22:24 +0100

    Code beauty

--- richpresence/client.lua
+++ richpresence/client.lua
@@ -1,32 +1,31 @@
 Citizen.CreateThread(function()
 	while true do
 		if Config.Enabled then
-			id = GetPlayerServerId(PlayerId())
 			SetDiscordAppId(Config.appID)
 			
-			if Config.showBigPhoto == true then
+			if Config.showBigPhoto then
 				SetDiscordRichPresenceAsset(Config.bigPhoto)
 			end
-			if Config.showSmallPhoto == true then
+			if Config.showSmallPhoto then
 				SetDiscordRichPresenceAssetSmall(Config.smallPhoto)
 			end
-			if Config.showBigAltText == true then
+			if Config.showBigAltText then
 				SetDiscordRichPresenceAssetText(Config.bigPhotoAltText)
 			end
-			if Config.showSmallAltText == true then
+			if Config.showSmallAltText then
 				SetDiscordRichPresenceAssetSmallText(Config.smallPhotoAltText)
 			end
 			
 			if Config.showID and Config.showName and Config.showPlayers then
-				SetRichPresence("[ID:"..id.."] - | [Name: ".. GetPlayerName(PlayerId()) .." | "..#GetActivePlayers().."/64 |")
+				SetRichPresence("[ID:"..GetPlayerServerId(PlayerId()).."] - | [Name: ".. GetPlayerName(PlayerId()) .." | "..#GetActivePlayers().."/64 |")
 			elseif Config.showPlayers ~= true and Config.showID and Config.showName then
-				SetRichPresence("[ID:"..id.."] - | [Name: ".. GetPlayerName(PlayerId()))
+				SetRichPresence("[ID:"..GetPlayerServerId(PlayerId()).."] - | [Name: ".. GetPlayerName(PlayerId()))
 			elseif Config.showPlayers and Config.showID ~= true and Config.showName then
 				SetRichPresence("[Name: ".. GetPlayerName(PlayerId()) .." | "..#GetActivePlayers().."/64 |")
 			elseif Config.showPlayers and Config.showID and Config.showName ~= true then
-				SetRichPresence("[ID:"..id.."] - | "..#GetActivePlayers().."/64 |")
+				SetRichPresence("[ID:"..GetPlayerServerId(PlayerId()).."] - | "..#GetActivePlayers().."/64 |")
 			elseif Config.showPlayers ~= true and Config.showName ~= true and Config.showID then
-				SetRichPresence("[ID:"..id.."] | ")
+				SetRichPresence("[ID:"..GetPlayerServerId(PlayerId()).."] | ")
 			elseif Config.showPlayers and Config.showName ~= true and Config.showID ~= true then
 				SetRichPresence(#GetActivePlayers().."/64 | ")
 			elseif Config.showID ~= true and Config.showName and Config.showPlayers ~= true then
